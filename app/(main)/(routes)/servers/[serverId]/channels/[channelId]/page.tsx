d38b9f710a89ddc67dba7fdb55b9aa8d3e653bf2
Max Chornenkyy <102178865+Handssoap@users.noreply.github.com>
2024-06-11 13:00:10 -0400

    added chat headers/chat in servers

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,3 +1,10 @@
+import { ChatHeader } from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
 interface ChannelIdPageProps {
     params: {
         serverId: string;
@@ -5,10 +12,45 @@ interface ChannelIdPageProps {
     }
 }
 
-const ChannelIdPage = () => {    
+const ChannelIdPage = async (
+    params: ChannelIdPageProps
+) => { 
+    const profile = await currentProfile();   
+    if(!profile) {
+        return redirectToSignIn();
+    }
+
+    const channel = await db.channel.findUnique({
+        where: {
+            id: params.params.channelId,
+        }
+        
+    })
+    const member = await db.member.findFirst({
+        where: {
+            serverId: params.params.serverId,
+            profileId: profile.id
+        }
+    })
+
+    if (!channel || !member) {
+        redirect("/")
+    }
     return (
-        <div>
-            Channelid
+        <div className="flex flex-col h-full">
+            <ChatHeader serverId={channel.serverId} name={channel.name} type="channel" />
+            <div className="flex-1">
+                Future Messages
+                </div>
+                <ChatInput 
+                name = {channel.name}
+                type="channel"
+                apiUrl="/api/socket/messages"
+                query={{
+                    channelId: channel.id,
+                    serverId: channel.serverId,
+                 }}/>
+            
         </div>
     );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import { cn } from '@/lib/utils'
 import { ModalProvider } from "@/components/providers/modal-provider";
+import { SocketProvider } from "@/components/providers/socket-provider";
 
 const inter = Open_Sans({ subsets: ["latin"] });
 
@@ -29,8 +30,10 @@ export default function RootLayout({
           defaultTheme="dark"
           enableSystem={false}
           storageKey="discord-theme">
+            <SocketProvider>
             <ModalProvider/>
-            {children}
+             {children}
+            </SocketProvider>
         </ThemeProvider>
         </body>
     </html>
